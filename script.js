d090c0fc63ceeb62640598350b31028af6e7f491
karache52NN <karache@yandex.ru>
2017-03-29 13:01:29 +0300

    battleships
    play battleships

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
-var string1="of beer on the wall";
-var bottles=" Bottles";
-var word=" of beer,";
-var count=99;
-while (count>0) {
-    console.log(count+" "+bottles+string1+"<br>");
-    console.log(count+" "+bottles+word+"<br>");
-    console.log("Take on down and pass it around<br>");
-    count=count-1;
-    if (count>0) {
-      console.log(count+" "+bottles+string1+"<br>");
-    }
-    else {
-      console.log("no more beer on the wall");
-    }
+var location1=0;
+var location2=0;
+var location3=0;
+var randlock=Math.floor(Math.random()*5);
+location1=randlock;
+location2=location1+1;
+location3=location2+1;
+
+var gues=0;
+var numberOfTry=0;
+var hits=0;
+var guesses=0;
+
+var isSunk=false;
+
+while (isSunk==false) {
+  gues=prompt("Ready! Aim! Fire!(enter a number 0-6):");
+  if (gues<0 || gues>6) {
+    alert("Please? enter a valid cell number");
+  }
+  else {
+    guesses=guesses+1;
+  }
+
+  if (gues==location1 || gues==location2 || gues==location3) {
+    hits=hits+1;
+    alert("Hit");
+  }
+  else {
+    alert("Miss");
+  }
+
+  if (hits==3) {
+    isSunk=true;
+    alert("You sank the battleship");
+  }
 }
-console.log(count);
+var stats="You took "+guesses+" guesses to sunk my battleship" +
+" wich means you accuracy was "+((3/guesses)*100)+"%";
+alert(stats);
