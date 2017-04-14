3483e6721274a8481b8358f34bbc4bf1c80e85c1
karache52NN <karache@yandex.ru>
2017-04-14 20:07:34 +0300

    chapter 9 complete

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ window.onload = init();
 function init() {
     var images = document.getElementsByTagName("img");
     for (var i = 0; i < images.length; i++) {
-        images[i].onclick = imgClickHandler(eventObj);
+        images[i].onclick = imgClickHandler;
     }
+    var hidimage=document.getElementById("zero3");
+    hidimage.onmouseover=imgClickHandler;
+    hidimage.onmouseout=blurElem;
+    /*setInterval(timerHandler, 5000);*/
 
 }
 
@@ -13,4 +17,13 @@ function imgClickHandler(eventObj) {
     var name= "img/zero.jpg";
     img.src=name;
 
+    setTimeout(timerHandler, 5000, img);
+
+}
+function blurElem(eventObj){
+  var img=eventObj.target;
+  img.src="img/zeroblur.jpg";
+}
+function timerHandler(img){
+  img.src="img/zeroblur.jpg";
 }
