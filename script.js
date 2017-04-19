53cbdb3857eb04719f19a79666c5d3d12ce12185
karache52NN <karache@yandex.ru>
2017-04-19 12:57:58 +0300

    замыкания пример

--- script.js
+++ script.js
@@ -1,29 +1,12 @@
-window.onload = init();
-
-function init() {
-    var images = document.getElementsByTagName("img");
-    for (var i = 0; i < images.length; i++) {
-        images[i].onclick = imgClickHandler;
-    }
-    var hidimage=document.getElementById("zero3");
-    hidimage.onmouseover=imgClickHandler;
-    hidimage.onmouseout=blurElem;
-    /*setInterval(timerHandler, 5000);*/
-
-}
-
-function imgClickHandler(eventObj) {
-    var img = eventObj.target;
-    var name= "img/zero.jpg";
-    img.src=name;
-
-    setTimeout(timerHandler, 5000, img);
-
-}
-function blurElem(eventObj){
-  var img=eventObj.target;
-  img.src="img/zeroblur.jpg";
-}
-function timerHandler(img){
-  img.src="img/zeroblur.jpg";
+window.onload = function() {
+  var count = 0;
+  var message = "You clicked me ";
+  var div = document.getElementById("message");
+  var button = document.getElementById("clickme");
+  button.onclick = function() {
+    var message = "You clicked me ";
+    var div = document.getElementById("message");
+    count++;
+    div.innerHTML = message + count + " times!";
+  };
 }
